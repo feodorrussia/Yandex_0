30c7f6baf3f98e25ce2980496d831b9e0f46b7c4
fyodorussia <fvdelows@mail.ru>
2019-05-14 11:34:23 +0300

    +регистр.

--- 1/total.py
+++ 1/total.py
@@ -1,6 +1,8 @@
+from register_form import RegisterModel
 from flask import Flask, url_for, session, redirect, render_template, request, jsonify, \
     make_response, \
     request
+from werkzeug.security import generate_password_hash
 import os
 import datetime
 from json import load
@@ -24,14 +26,39 @@ def title():
                            username=session['username'])
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    pass
+    if request.method == 'GET':
+        if 'username' not in session:
+            if "username" in session:
+                return redirect("/logout")
+            return redirect('/login')
+        return render_template('login.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
-    pass
+    # if 'username' in session:
+    #     return redirect('/')
+    form = RegisterModel()
+    if form.validate_on_submit():
+        user_name = form.user_name.data
+        password = form.password.data
+        password_check = form.password_check.data
+        user = ''
+        flag = user.is_username_busy(user_name)
+        if flag and password == password_check:
+            user.insert(user_name, password)
+            session['username'] = user_name
+            hash = generate_password_hash(password)
+            exists = user.exists(user_name, password)
+            session['user_id'] = exists[1]
+            return render_template('register.html', form=form, error=0)
+        elif password != password_check:
+            return render_template('register.html', form=form, error=2)
+        else:
+            return render_template('register.html', form=form, error=1)
+    return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
